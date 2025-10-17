e97374854a75d42b29c23edb1336e46a4ce7aeed
mansoor <manzooravail@gmail.com>
2025-10-17 14:49:08 +0530

    fix: script issues

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 
-replaceText(document.body)
-
 function replaceText(element) {
-  if (element.hasChildNodes() {
+  if (element.hasChildNodes()) {
     element.childNodes.forEach(replaceText)
-  } else if (element.nodeType === Text.TEXT_NODE) {
+  } else if (element.nodeType === Node.TEXT_NODE) {
     if (element.textContent.match(/coronavirus/gi)) {
       const newElement = document.createElement('span')
       newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi, '<span class="rainbow">$1</span>')
@@ -12,3 +10,5 @@ function replaceText(element) {
     }
   }
 }
+
+replaceText(document.body);
